6267196bb9fc2c8c413216a15038724a32e6c593
Manh Pham <manhpham90vn@icloud.com>
2020-04-06 00:09:17 +0700

    Initial Commit

---
+++ MPDebug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MPDebug
+//
+//  Created by Manh Pham on 4/6/20.
+//  Copyright © 2020 Manh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
